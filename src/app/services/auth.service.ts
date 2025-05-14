2a266da0a487ab99b4e37c657ad3eefd9283123a
lovekesharya <lovekesh.arya@nagarro.com>
2025-05-14 12:03:51 +0530

    offloaded the Authentication related work to AuthService

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: AngularFireAuth, private firestore: AngularFirestore) { }
+
+  public async createUser(userData: any) {
+   const {email,password,name,age,phoneNumber} = userData;
+   await this.auth.createUserWithEmailAndPassword(email, password)
+         .then((userCredential) => {
+            this.firestore.collection('users').add({
+               name: name.value,
+               email: email.value,
+               age: age.value,
+               phoneNumber: phoneNumber.value,
+            })
+         }
+         ).catch((error) => {
+            throw error;
+         });
+  }
+}
+// Rather than teaching the component how to use the angularfire library services, we will create a centralized logic and inject into components for creating a user 
+//we are adding a abstraction between the component and library services 
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { RegisterValidators } from '../validators/register-validators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from '../../services/auth.service';
+import IUser from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -15,12 +17,12 @@ export class RegisterComponent {
    showAlert = false;
    isRequestProcessing = false; //if the network is slow, then this will not let the user to submit the form multiple times
 
-   constructor(private auth: AngularFireAuth, private firestore: AngularFirestore) {
+   constructor(private authService: AuthService) {
    }
 
    name = new FormControl('', [Validators.required, Validators.minLength(3)]);
    email = new FormControl('', [Validators.required, Validators.email]);
-   age = new FormControl('', [Validators.required]);
+   age = new FormControl<number | null>(null, [Validators.required]); // the type of the form control can be configure using generic type
    password = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),]);
    confirmPassword = new FormControl('', [Validators.required]);
    phoneNumber = new FormControl('', [Validators.required, Validators.minLength(12), Validators.maxLength(12)]);
@@ -37,34 +39,30 @@ export class RegisterComponent {
    [RegisterValidators.match('password', 'confirmPassword')]);
 
    async onSubmit() {
+      //whenerver user clicks on the submit button, we will show the alert message and if the request is taking time, we will show the alert message and user click on the submit button again, then this blue message will be shown
+      this.showAlert = true;
+      this.alertMssg = 'Your Request is being processed, we will be registering you soon..';
+      this.alertColor = 'blue';
       this.isRequestProcessing = true;
       this.showAlert = true;
-      const {email, password} = this.registerForm.value;
-      await this.auth.createUserWithEmailAndPassword(email as string, password as string)
-         .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            this.alertMssg = 'User registered successfully';
-            this.alertColor = 'green';
-            this.showAlert = true;
-            console.log('userCred',user);
-            this.firestore.collection('users').add({
-               name: this.name.value,
-               email: this.email.value,
-               age: this.age.value,
-               phoneNumber: this.phoneNumber.value,
-            })
-         }
-         ).catch((error) => {
-            this.isRequestProcessing = false;
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            this.alertMssg = errorMessage;
-            this.alertColor = 'red';
-            this.showAlert = true;
-            console.log(errorCode);
-            console.log(errorMessage);
-         }
-         );
+      const userData = this.registerForm.value;
+
+      
+      try{
+         await this.authService.createUser(userData as IUser);
+      }
+      catch (error) {
+         this.isRequestProcessing = false;
+         const errorCode = (error as any).code;
+         const errorMessage = (error as any).message;
+         this.alertMssg = errorMessage;
+         this.alertColor = 'red';
+         this.showAlert = true;
+         console.log(errorCode);
+         console.log(errorMessage);
+         return;
+      }
+      this.alertMssg = 'User registered successfully';
+      this.alertColor = 'green';
    }
 }
