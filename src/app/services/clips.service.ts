8e6b36453874f9356cb2a4201f899d27e1e31885
lovekesharya <lovekesh.arya@nagarro.com>
2025-05-21 19:24:21 +0530

    added behavioural subject for sorting the clips

--- src/app/services/clips.service.ts
+++ src/app/services/clips.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
 import { AuthService } from './auth.service';
-import { of, switchMap } from 'rxjs';
+import { of, switchMap, BehaviorSubject, combineLatest } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 @Injectable({
   providedIn: 'root'
@@ -18,19 +18,21 @@ export class ClipsService {
       return this.clipsCollection.add(clip);
    }
 
-   getUserClips(){
-      return this.authService.getUser$.pipe(
-         switchMap((user) => {
+   getUserClips(sort$: BehaviorSubject<string>){
+      return combineLatest([this.authService.getUser$, sort$]).pipe(
+         switchMap((values) => {
+            const [user, sort] = values;
             if (!user) {
                return of([]);
             } else {
-               return this.clipsCollection.ref.where('uid', '==', user.uid).get().then(snapshot => 
+               return this.clipsCollection.ref.where('uid', '==', user.uid).orderBy('timestamp', sort === '1'? 'desc': 'asc').get().then(snapshot => 
                   snapshot.docs.map(doc => ({docID: doc.id, ...doc.data() as IClip})),
                );
             }
          })
       );
    }
+   //combine latest helps to subscribe both observable simultaneously
 
    updateClips(id: string, title: string){
       this.clipsCollection.doc(id).update({
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipsService } from 'src/app/services/clips.service';
 import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -13,8 +14,9 @@ export class ManageComponent implements OnInit {
    videoOrder = '1';
    clips: IClip[] = [];
    activeClip: IClip | null = null;
+   sort$: BehaviorSubject<string>
    constructor(private router: Router, private route: ActivatedRoute, private clipsService: ClipsService, private modalService: ModalService){
-      
+      this.sort$ = new BehaviorSubject(this.videoOrder);
    }
    ngOnInit(): void {
       this.route.queryParamMap.subscribe((params : Params) =>{
@@ -22,10 +24,11 @@ export class ManageComponent implements OnInit {
          //it might be possible that change in any other query parameter can cause the observable to push a new value
          // this.videoOrder = params.sort === '2' ? params.sort : '1' //so we are delibarately check the value
          const sort = params.get('sort');      // ← use get()
-         this.videoOrder = sort === '2' ? sort! : '1';
+         this.videoOrder = sort === '2' ? sort! : '1'; // '!' is a non-null assertion operator in TypeScript, guaranteed that sort is not null or undefined here
+         this.sort$.next(this.videoOrder);
       })
       
-      this.clipsService.getUserClips().subscribe((data) =>{
+      this.clipsService.getUserClips(this.sort$).subscribe((data) =>{
          this.clips = data;
       });
 
