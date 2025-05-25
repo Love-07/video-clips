a6f1f5aac5842a3c88e6a434be29d187837d9631
lovekesharya <lovekesh.arya@nagarro.com>
2025-05-25 13:26:04 +0530

    added the pipe for bypassing sanitizing though it is not needed

--- src/app/video/pipes/safe-url.pipe.ts
+++ src/app/video/pipes/safe-url.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
   name: 'safeURL'
 })
 export class SafeURLPipe implements PipeTransform {
+   constructor(private sanitizer : DomSanitizer){
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+   }
+
+  transform(value: string){
+    // I will not be using this as my screeshot are already being displayed without adding this code, so no need to add this bypass kind of this which can be danger in terms of security reasons
+    //return this.sanitizer.bypassSecurityTrustUrl(value);
   }
 
 }
